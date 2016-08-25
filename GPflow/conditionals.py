5318b8a6ff1010d7fa49f9b2d783757c0d818ba3
alexggmatthews <alexggmatthews@googlemail.com>
2016-08-25 17:04:07 +0100

    Revert "use batch ops in KLs and conditionals (#159)"
    This reverts commit b3dab0288ddcd165e2ba6f95061b5f3d7bf82a1a.

--- GPflow/conditionals.py
+++ GPflow/conditionals.py
@@ -1,11 +1,11 @@
 # Copyright 2016 Valentine Svensson, James Hensman, alexggmatthews
-#
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+# 
 # http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,7 +17,8 @@ from .tf_hacks import eye
 import tensorflow as tf
 
 
-def conditional(Xnew, X, kern, f, full_cov=False, q_sqrt=None, whiten=False):
+def conditional(Xnew, X, kern, f, num_columns,
+                full_cov=False, q_sqrt=None, whiten=False):
     """
     Given F, representing the GP at the points X, produce the mean and
     (co-)variance of the GP at the points Xnew.
@@ -42,7 +43,9 @@ def conditional(Xnew, X, kern, f, full_cov=False, q_sqrt=None, whiten=False):
      - Xnew is a data matrix, size N x D
      - X are data points, size M x D
      - kern is a GPflow kernel
-     - f is a data matrix, M x K, representing the function values at X, for K functions.
+     - f is a data matrix, M x K, representing the function values at X.
+     - num_columns is an integer number of columns in the f matrix (must match
+       q_sqrt's last dimension)
      - q_sqrt (optional) is a matrix of standard-deviations or Cholesky
        matrices, size M x K or M x M x K
      - whiten (optional) is a boolean: whether to whiten the representation
@@ -67,12 +70,11 @@ def conditional(Xnew, X, kern, f, full_cov=False, q_sqrt=None, whiten=False):
 
     # compute the covariance due to the conditioning
     if full_cov:
-        fvar = kern.K(Xnew) - tf.matmul(A, A, transpose_a=True)
-        shape = tf.pack([tf.shape(f)[1], 1, 1])
+        fvar = kern.K(Xnew) - tf.matmul(tf.transpose(A), A)
+        fvar = tf.tile(tf.expand_dims(fvar, 2), [1, 1, num_columns])
     else:
         fvar = kern.Kdiag(Xnew) - tf.reduce_sum(tf.square(A), 0)
-        shape = tf.pack([tf.shape(f)[1], 1])
-    fvar = tf.tile(tf.expand_dims(fvar, 0), shape)  # D x N x N or D x N
+        fvar = tf.tile(tf.expand_dims(fvar, 1), [1, num_columns])
 
     # another backsubstitution in the unwhitened case
     if not whiten:
@@ -81,21 +83,23 @@ def conditional(Xnew, X, kern, f, full_cov=False, q_sqrt=None, whiten=False):
     # construct the conditional mean
     fmean = tf.matmul(tf.transpose(A), f)
 
+    # add extra projected variance from q(f) if needed
     if q_sqrt is not None:
-        if q_sqrt.get_shape().ndims == 2:
-            LTA = A * tf.expand_dims(tf.transpose(q_sqrt), 2)  # D x M x N
-        elif q_sqrt.get_shape().ndims == 3:
-            L = tf.batch_matrix_band_part(tf.transpose(q_sqrt, (2, 0, 1)), -1, 0)  # D x M x M
-            A_tiled = tf.tile(tf.expand_dims(A, 0), tf.pack([tf.shape(f)[1], 1, 1]))
-            LTA = tf.batch_matmul(L, A_tiled, adj_x=True)  # D x M x N
-        else:  # pragma: no cover
-            raise ValueError("Bad dimension for q_sqrt: %s" %
-                             str(q_sqrt.get_shape().ndims))
-        if full_cov:
-            fvar = fvar + tf.batch_matmul(LTA, LTA, adj_x=True)  # D x N x N
-        else:
-            fvar = fvar + tf.reduce_sum(tf.square(LTA), 1)  # D x N
-    fvar = tf.transpose(fvar)  # N x D or N x N x D
+        projected_var = []
+        for d in range(num_columns):
+            if q_sqrt.get_shape().ndims == 2:
+                LTA = A * q_sqrt[:, d:d + 1]
+            elif q_sqrt.get_shape().ndims == 3:
+                L = tf.batch_matrix_band_part(q_sqrt[:, :, d], -1, 0)
+                LTA = tf.matmul(tf.transpose(L), A)
+            else:  # pragma no cover
+                raise ValueError("Bad dimension for q_sqrt: %s" %
+                                 str(q_sqrt.get_shape().ndims))
+            if full_cov:
+                projected_var.append(tf.matmul(tf.transpose(LTA), LTA))
+            else:
+                projected_var.append(tf.reduce_sum(tf.square(LTA), 0))
+        fvar = fvar + tf.transpose(tf.pack(projected_var))
 
     return fmean, fvar
 
@@ -106,7 +110,7 @@ import warnings
 def gp_predict(Xnew, X, kern, F, full_cov=False):
     warnings.warn('gp_predict is deprecated: use conditonal(...) instead',
                   DeprecationWarning)
-    return conditional(Xnew, X, kern, F,
+    return conditional(Xnew, X, kern, F, num_columns=1,
                        full_cov=full_cov, q_sqrt=None, whiten=False)
 
 
@@ -114,7 +118,7 @@ def gaussian_gp_predict(Xnew, X, kern, q_mu, q_sqrt, num_columns,
                         full_cov=False):
     warnings.warn('gp_predict is deprecated: use conditonal(...) instead',
                   DeprecationWarning)
-    return conditional(Xnew, X, kern, q_mu,
+    return conditional(Xnew, X, kern, q_mu, num_columns=num_columns,
                        full_cov=full_cov, q_sqrt=q_sqrt, whiten=False)
 
 
@@ -122,12 +126,12 @@ def gaussian_gp_predict_whitened(Xnew, X, kern, q_mu, q_sqrt, num_columns,
                                  full_cov=False):
     warnings.warn('gp_predict is deprecated: use conditonal(...) instead',
                   DeprecationWarning)
-    return conditional(Xnew, X, kern, q_mu,
+    return conditional(Xnew, X, kern, q_mu, num_columns=num_columns,
                        full_cov=full_cov, q_sqrt=q_sqrt, whiten=True)
 
 
 def gp_predict_whitened(Xnew, X, kern, V, full_cov=False):
     warnings.warn('gp_predict is deprecated: use conditonal(...) instead',
                   DeprecationWarning)
-    return conditional(Xnew, X, kern, V,
+    return conditional(Xnew, X, kern, V, num_columns=1,
                        full_cov=full_cov, q_sqrt=None, whiten=True)
--- GPflow/kullback_leiblers.py
+++ GPflow/kullback_leiblers.py
@@ -1,11 +1,11 @@
 # Copyright 2016 James Hensman, alexggmatthews
-#
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+# 
 # http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,7 +17,7 @@ import tensorflow as tf
 from .tf_hacks import eye
 
 
-def gauss_kl_white(q_mu, q_sqrt):
+def gauss_kl_white(q_mu, q_sqrt, num_latent):
     """
     Compute the KL divergence from
 
@@ -25,23 +25,28 @@ def gauss_kl_white(q_mu, q_sqrt):
     to
           p(x) = N(0, I)
 
-    We assume multiple independent distributions, given by the columns of
+    We assume num_latent independent distributions, given by the columns of
     q_mu and the last dimension of q_sqrt.
 
     q_mu is a matrix, each column contains a mean
 
     q_sqrt is a 3D tensor, each matrix within is a lower triangular square-root
         matrix of the covariance.
+
+    num_latent is an integer: the number of independent distributions (equal to
+        the columns of q_mu and the last dim of q_sqrt).
     """
     KL = 0.5 * tf.reduce_sum(tf.square(q_mu))  # Mahalanobis term
-    KL += -0.5 * tf.cast(tf.reduce_prod(tf.shape(q_sqrt)[1:]), tf.float64)  # constant term
-    L = tf.batch_matrix_band_part(tf.transpose(q_sqrt, (2, 0, 1)), -1, 0)  # force lower triangle
-    KL -= 0.5 * tf.reduce_sum(tf.log(tf.square(tf.batch_matrix_diag_part(L))))  # logdet
-    KL += 0.5 * tf.reduce_sum(tf.square(L))  # Trace term.
+    KL += -0.5 * tf.cast(tf.shape(q_sqrt)[0] * num_latent, tf.float64)
+    for d in range(num_latent):
+        Lq = tf.batch_matrix_band_part(q_sqrt[:, :, d], -1, 0)
+        # Log determinant of q covariance:
+        KL -= 0.5 * tf.reduce_sum(tf.log(tf.square(tf.diag_part(Lq))))
+        KL += 0.5 * tf.reduce_sum(tf.square(Lq))  # Trace term.
     return KL
 
 
-def gauss_kl_white_diag(q_mu, q_sqrt):
+def gauss_kl_white_diag(q_mu, q_sqrt, num_latent):
     """
     Compute the KL divergence from
 
@@ -49,23 +54,26 @@ def gauss_kl_white_diag(q_mu, q_sqrt):
     to
           p(x) = N(0, I)
 
-    We assume multiple independent distributions, given by the columns of
+    We assume num_latent independent distributions, given by the columns of
     q_mu and q_sqrt
 
     q_mu is a matrix, each column contains a mean
 
     q_sqrt is a matrix, each column represents the diagonal of a square-root
         matrix of the covariance.
+
+    num_latent is an integer: the number of independent distributions (equal to
+        the columns of q_mu and q_sqrt).
     """
 
     KL = 0.5 * tf.reduce_sum(tf.square(q_mu))  # Mahalanobis term
-    KL += -0.5 * tf.cast(tf.size(q_sqrt), tf.float64)
+    KL += -0.5 * tf.cast(tf.shape(q_sqrt)[0] * num_latent, tf.float64)
     KL += -0.5 * tf.reduce_sum(tf.log(tf.square(q_sqrt)))  # Log-det of q-cov
     KL += 0.5 * tf.reduce_sum(tf.square(q_sqrt))  # Trace term
     return KL
 
 
-def gauss_kl_diag(q_mu, q_sqrt, K):
+def gauss_kl_diag(q_mu, q_sqrt, K,  num_latent):
     """
     Compute the KL divergence from
 
@@ -73,7 +81,7 @@ def gauss_kl_diag(q_mu, q_sqrt, K):
     to
           p(x) = N(0, K)
 
-    We assume multiple independent distributions, given by the columns of
+    We assume num_latent independent distributions, given by the columns of
     q_mu and q_sqrt.
 
     q_mu is a matrix, each column contains a mean
@@ -82,14 +90,16 @@ def gauss_kl_diag(q_mu, q_sqrt, K):
         matrix of the covariance of q.
 
     K is a positive definite matrix: the covariance of p.
+
+    num_latent is an integer: the number of independent distributions (equal to
+        the columns of q_mu and q_sqrt).
     """
     L = tf.cholesky(K)
     alpha = tf.matrix_triangular_solve(L, q_mu, lower=True)
     KL = 0.5 * tf.reduce_sum(tf.square(alpha))  # Mahalanobis term.
-    num_latent = tf.cast(tf.shape(q_sqrt)[1], tf.float64)
     KL += num_latent * 0.5 * tf.reduce_sum(
         tf.log(tf.square(tf.diag_part(L))))  # Prior log-det term.
-    KL += -0.5 * tf.cast(tf.size(q_sqrt), tf.float64)  # constant term
+    KL += -0.5 * tf.cast(tf.shape(q_sqrt)[0] * num_latent, tf.float64)
     KL += -0.5 * tf.reduce_sum(tf.log(tf.square(q_sqrt)))  # Log-det of q-cov
     L_inv = tf.matrix_triangular_solve(L, eye(tf.shape(L)[0]), lower=True)
     K_inv = tf.matrix_triangular_solve(tf.transpose(L), L_inv, lower=False)
@@ -98,7 +108,7 @@ def gauss_kl_diag(q_mu, q_sqrt, K):
     return KL
 
 
-def gauss_kl(q_mu, q_sqrt, K):
+def gauss_kl(q_mu, q_sqrt, K, num_latent):
     """
     Compute the KL divergence from
 
@@ -106,7 +116,7 @@ def gauss_kl(q_mu, q_sqrt, K):
     to
           p(x) = N(0, K)
 
-    We assume multiple independent distributions, given by the columns of
+    We assume num_latent independent distributions, given by the columns of
     q_mu and the last dimension of q_sqrt.
 
     q_mu is a matrix, each column contains a mean.
@@ -115,16 +125,20 @@ def gauss_kl(q_mu, q_sqrt, K):
         matrix of the covariance of q.
 
     K is a positive definite matrix: the covariance of p.
+
+    num_latent is an integer: the number of independent distributions (equal to
+        the columns of q_mu and the last dim of q_sqrt).
     """
     L = tf.cholesky(K)
     alpha = tf.matrix_triangular_solve(L, q_mu, lower=True)
     KL = 0.5 * tf.reduce_sum(tf.square(alpha))  # Mahalanobis term.
-    num_latent = tf.cast(tf.shape(q_sqrt)[2], tf.float64)
-    KL += num_latent * 0.5 * tf.reduce_sum(tf.log(tf.square(tf.diag_part(L))))  # Prior log-det term.
-    KL += -0.5 * tf.cast(tf.reduce_prod(tf.shape(q_sqrt)[1:]), tf.float64)  # constant term
-    Lq = tf.batch_matrix_band_part(tf.transpose(q_sqrt, (2, 0, 1)), -1, 0)  # force lower triangle
-    KL += -0.5*tf.reduce_sum(tf.log(tf.square(tf.batch_matrix_diag_part(Lq))))  # logdet
-    L_tiled = tf.tile(tf.expand_dims(L, 0), tf.pack([tf.shape(Lq)[0], 1, 1]))
-    LiLq = tf.batch_matrix_triangular_solve(L_tiled, Lq, lower=True)
-    KL += 0.5 * tf.reduce_sum(tf.square(LiLq))  # Trace term
+    KL += num_latent * 0.5 * tf.reduce_sum(
+        tf.log(tf.square(tf.diag_part(L))))  # Prior log-det term.
+    KL += -0.5 * tf.cast(tf.shape(q_sqrt)[0] * num_latent, tf.float64)
+    for d in range(num_latent):
+        Lq = tf.batch_matrix_band_part(q_sqrt[:, :, d], -1, 0)
+        # Log determinant of q covariance:
+        KL += -0.5*tf.reduce_sum(tf.log(tf.square(tf.diag_part(Lq))))
+        LiLq = tf.matrix_triangular_solve(L, Lq, lower=True)
+        KL += 0.5 * tf.reduce_sum(tf.square(LiLq))  # Trace term
     return KL
